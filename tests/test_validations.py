d7c49badd10b6e0658e87d79d1cc6e432266c73a
Dan Lewis-Toakley <dan.lewistoakley@gmail.com>
2019-07-03 11:52:09 -0400

    moved validations and assosiated tests to test dir

--- tests/test_validations.py
+++ tests/test_validations.py
@@ -1,14 +1,12 @@
 import pytest
-import validations
+from invisible_flow import validations
 
 
 @pytest.mark.parametrize('filename', ['1.xls', '2.csv', '3.xlsx'])
 def test_should_return_true_for_valid_extensions(filename):
-    is_valid = validations.are_valid_file_extensions(filename)
-    assert is_valid is True
+    assert validations.are_valid_file_extensions(filename)
 
 
 @pytest.mark.parametrize('filename', ['1.doc', '2.txt', '3.dat'])
 def test_should_return_false_for_invalid_extensions(filename):
-    is_valid = validations.are_valid_file_extensions(filename)
-    assert is_valid is False
+    assert not validations.are_valid_file_extensions(filename)
