92f0ef71c7bfeb060141785f03b4e889a9c2d13e
Adi <adinaths@gmail.com>
2019-07-03 11:22:35 -0400

    format app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def fileFrontPage():
     return render_template('./index.html')
@@ -13,10 +14,11 @@ def handleFileUpload():
     if 'foia' in request.files:
         foia = request.files['foia']
         if foia.filename != '':
-        	# replace with s3
-        	with open('uploads/' + foia.filename, 'wb') as f:
-        		f.write(foia)
+            # replace with s3
+            with open('uploads/' + foia.filename, 'wb') as f:
+                f.write(foia)
     return redirect(url_for('fileFrontPage'))
 
+
 if __name__ == '__main__':
-    app.run()     
+    app.run()
