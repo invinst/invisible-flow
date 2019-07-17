3ccffaadaca4d4aee307ef3db01dc13a00f6153d
terrencebunkley <terrence.bunkley@thoughtworks.com>
2019-07-17 14:10:47 -0500

    Added logging to files

--- invisible_flow/app.py
+++ invisible_flow/app.py
@@ -1,10 +1,32 @@
+from logging import getLogger
+from logging.config import dictConfig
+
 from flask import Flask, render_template, Response, Request
 
 from invisible_flow.globals_factory import GlobalsFactory
 from invisible_flow.storage.storage_factory import StorageFactory
 from invisible_flow.validation import is_valid_file_type
 
+# Logging configuration
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://sys.stdout',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
+
+logger = getLogger(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -17,18 +39,21 @@ def foia_response_upload():
     request_context: Request = GlobalsFactory.get_request_context()
 
     if 'multipart/form-data' not in request_context.content_type:
+        logger.error("Unsupported media type uploaded to FOIA. content type={}".format(request_context.content_type))
         return Response(status=415, response='Unsupported media type. Please upload a .csv .xls or .xlsx file.')
 
     foia_response_file = request_context.files['foia_response']
     if not is_valid_file_type(foia_response_file.filename):
+        logger.error("Unsupported file type uploaded to FOIA. filename={}".format(foia_response_file.filename))
         return Response(status=415, response='Unsupported file type. Please upload a .csv .xls or .xlsx file.')
 
     storage = StorageFactory.get_storage()
     response_type = request_context.form['response_type']
-    todays_date = GlobalsFactory.get_current_datetime_utc().isoformat(sep="_").replace(':', '-')
+    current_date = GlobalsFactory.get_current_datetime_utc().isoformat(sep="_").replace(':', '-')
 
-    storage.store("{}.csv".format(response_type), foia_response_file, "ui-{}/initial_data".format(todays_date))
+    storage.store("{}.csv".format(response_type), foia_response_file, "ui-{}/initial_data".format(current_date))
 
+    logger.info("Successfully uploaded FOIA file")
     return Response(status=200, response='Success')
 
 
--- invisible_flow/storage/gcs_storage.py
+++ invisible_flow/storage/gcs_storage.py
@@ -1,4 +1,5 @@
 import os
+from logging import getLogger
 
 from google.cloud.storage import Client
 from google.api_core.exceptions import GoogleAPICallError
@@ -6,8 +7,14 @@ from werkzeug.datastructures import FileStorage
 
 from invisible_flow.storage.storage_base import StorageBase
 
+logger = getLogger(__name__)
+
 
 class GCStorage(StorageBase):
+
+    def __init__(self, gcs_client: Client):
+        self.gcs_client = gcs_client
+        self.bucket = gcs_client.bucket(os.environ.get('GCS_BUCKET'))
 
     def store(self, filename, file: FileStorage, path):
         blob = self.bucket.blob(os.path.join(path, filename))
@@ -21,8 +28,4 @@ class GCStorage(StorageBase):
         try:
             blob.upload_from_string(metadata_text)
         except GoogleAPICallError as error:
-            print(error)
-
-    def __init__(self, gcs_client: Client):
-        self.gcs_client = gcs_client
-        self.bucket = gcs_client.bucket(os.environ.get('GCS_BUCKET'))
+            logger.error('Error uploading metadata to GCS. message:{}'.format(error.message))
