60833349803b62cbf991ba9a84f1fadfa6b3baaa
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-07-22 21:20:56 -0500

    fix for response type data format

--- invisible_flow/transformers/transformer_factory.py
+++ invisible_flow/transformers/transformer_factory.py
@@ -5,7 +5,7 @@ class TransformerFactory:
 
     @staticmethod
     def get_transformer(response_type):
-        if response_type == 'Case Info':
+        if response_type == 'case_info':
             return CaseInfoAllegationsTransformer()
         else:
             raise Exception('Unable to handle this type of file')
