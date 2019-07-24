f4e582664bad1bac82fce014d6996ae29c0e3c89
Peyton Cross <crosspeyton@gmail.com>
2019-07-24 16:51:51 -0700

    Added transform csv to entity method

--- invisible_flow/transformers/investigator_transformer.py
+++ invisible_flow/transformers/investigator_transformer.py
@@ -1,10 +1,23 @@
 from invisible_flow.transformers.transformer_base import TransformerBase
+from invisible_flow.entities.data_investigator import Investigator
+import pandas as pd
+from io import StringIO
 
 
 class InvestigatorTransformer(TransformerBase):
+    def row_to_investigator(df):
+        return Investigator(last_name=df['INVST_LAST_NAME'],
+                            first_name=df['INVST_FIRST_NAME'],
+                            middle_initial=df['MIDDLE_INITIAL'],
+                            gender=df['SEX'],
+                            race=df['RACE'],
+                            appointed_date=df['APPOINTED_DATE'],
+                            officer_id=0)
 
-    def transform_investigator_csv_to_entity_list(self):
-        pass
+    def transform_investigator_csv_to_entity_list(csv_content):
+        csv_content_IO = StringIO(csv_content)
+        df = pd.read_csv(csv_content_IO)
+        return [InvestigatorTransformer.row_to_investigator(row) for _, row in df.iterrows()]
 
     def transform(self, response_type, file_content: str):
-        return self.transform_investigator_csv_to_entity_list()
+        return InvestigatorTransformer.transform_investigator_csv_to_entity_list(csv_content=file_content)
