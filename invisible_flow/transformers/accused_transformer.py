58f459017247786f7d0ded503a3662919139a2f3
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-07-31 14:42:07 -0500

    remove csv to entities function and tests, code cleanup

--- invisible_flow/transformers/accused_transformer.py
+++ invisible_flow/transformers/accused_transformer.py
@@ -3,67 +3,12 @@ from typing import List, Tuple
 import pandas as pd
 from io import StringIO
 
-from invisible_flow.entities.data_allegationcategory import AllegationCategory
-from invisible_flow.entities.data_officer import Officer
-from invisible_flow.entities.data_officerbadgenumber import OfficerBadgeNumber
-from invisible_flow.entities.data_policeunit import PoliceUnit
-
 from invisible_flow.transformers.transformer_base import TransformerBase
 
 
 class AccusedTransformer(TransformerBase):
 
-    def csv_to_entity_name_entity_list_tuples(self, csv_content) -> List[Tuple]:
-        # what if we push each entity into a collection for use later
-        # loop across rows of csv_content
-        # 4 collections one for each entity
-        # return a list of tuples [(name, [entity, entity]), (name, [entity, entity])]
-        string_io_csv = StringIO(csv_content)
-        accused_df = pd.read_csv(string_io_csv)
-        # doing it this way leads to each entity having all the data for each column in a single
-        # entity property, so category has all the rows ALLEGATION_CATEGORY entries in a single
-        # entity instance, I'm not so sure this will plug into the db correctly
-        return [
-            (
-                "allegationcategory",
-                AllegationCategory(
-                    category=str(accused_df['ALLEGATION_CATEGORY']),
-                    category_code=str(accused_df['ALLEGATION_CATEGORY_CD']),
-                    cr_id=str(accused_df['LOG_NO'])
-                )
-            ),
-            (
-                "officer",
-                Officer(
-                    birth_year=str(accused_df['BIRTH_YEAR']),
-                    first_name=str(accused_df['OFFICER_FIRST_NAME']),
-                    gender=str(accused_df['SEX_CODE_CD']),
-                    last_name=str(accused_df['OFFICER_LAST_NAME']),
-                    middle_initial=str(accused_df['MIDDLE_INITIAL']),
-                    race=str(accused_df['RACE_CODE_CD']),
-                    rank=str(accused_df['EMPLOYEE_POSITION']),
-                    cr_id=str(accused_df['LOG_NO'])
-                )
-            ),
-            (
-                "officerbadgenumber",
-                OfficerBadgeNumber(
-                    officer_id=str(accused_df['EMPLOYEE_NO']),
-                    star=str(accused_df['STAR_NO']),
-                    cr_id=str(accused_df['LOG_NO'])
-                )
-            ),
-            (
-                "policeunit",
-                PoliceUnit(
-                    tags=str(accused_df['EMPLOYEE_DETAIL_UNIT']),
-                    unit_name=str(accused_df['EMPLOYEE_ASSIGN_UNIT']),
-                    cr_id=str(accused_df['LOG_NO'])
-                )
-            )
-        ]
-
-    def csv_to_entity_name_csv_tuples(self, csv_content) -> List[Tuple]:
+    def csv_to_entity_name_csv_tuples(self, csv_content) -> List[Tuple[str, str]]:
         string_io_csv = StringIO(csv_content)
         accused_df = pd.read_csv(string_io_csv)
         allegationcategory_columns = [
@@ -95,35 +40,35 @@ class AccusedTransformer(TransformerBase):
             (
                 'allegationcategory',
                 accused_df.loc[:, allegationcategory_columns].rename(columns={
-                    'ALLEGATION_CATEGORY': 'category',
-                    'ALLEGATION_CATEGORY_CD': 'category_code',
-                    'LOG_NO': 'cr_id'
+                    allegationcategory_columns[0]: 'category',
+                    allegationcategory_columns[1]: 'category_code',
+                    allegationcategory_columns[2]: 'cr_id'
                 }).to_csv(index=False)
             ), (
                 'officer',
                 accused_df.loc[:, officer_columns].rename(columns={
-                    'BIRTH_YEAR': 'birth_year',
-                    'OFFICER_FIRST_NAME': 'first_name',
-                    'SEX_CODE_CD': 'gender',
-                    'OFFICER_LAST_NAME': 'last_name',
-                    'MIDDLE_INITIAL': 'middle_initial',
-                    'RACE_CODE_CD': 'race',
-                    'EMPLOYEE_POSITION': 'rank',
-                    'LOG_NO': 'cr_id'
+                    officer_columns[0]: 'birth_year',
+                    officer_columns[1]: 'first_name',
+                    officer_columns[2]: 'gender',
+                    officer_columns[3]: 'last_name',
+                    officer_columns[4]: 'middle_initial',
+                    officer_columns[5]: 'race',
+                    officer_columns[6]: 'rank',
+                    officer_columns[7]: 'cr_id'
                 }).to_csv(index=False)
             ), (
                 'officerbadgenumber',
                 accused_df.loc[:, officerbadgenumber_columns].rename(columns={
-                    'EMPLOYEE_NO': 'officer_id',
-                    'STAR_NO': 'star',
-                    'LOG_NO': 'cr_id'
+                    officerbadgenumber_columns[0]: 'officer_id',
+                    officerbadgenumber_columns[1]: 'star',
+                    officerbadgenumber_columns[2]: 'cr_id'
                 }).to_csv(index=False)
             ), (
                 'policeunit',
                 accused_df.loc[:, policeunit_columns].rename(columns={
-                    'EMPLOYEE_DETAIL_UNIT': 'tags',
-                    'EMPLOYEE_ASSIGN_UNIT': 'unit_name',
-                    'LOG_NO': 'cr_id'
+                    policeunit_columns[0]: 'tags',
+                    policeunit_columns[1]: 'unit_name',
+                    policeunit_columns[2]: 'cr_id'
                 }).to_csv(index=False)
             )
         ]
--- tests/transformers/test_accusedTransformer.py
+++ tests/transformers/test_accusedTransformer.py
@@ -2,69 +2,10 @@ import os
 
 from invisible_flow.transformers.accused_transformer import AccusedTransformer
 
-from invisible_flow.entities.data_allegationcategory import AllegationCategory
-from invisible_flow.entities.data_officer import Officer
-from invisible_flow.entities.data_officerbadgenumber import OfficerBadgeNumber
-from invisible_flow.entities.data_policeunit import PoliceUnit
-
-
 from tests.helpers.if_test_base import IFTestBase
 
 
 class TestAccusedTransformer:
-
-    def test_csv_to_entity_name_entity_list_tuples_head(self):
-        transformer = AccusedTransformer()
-        head_accused_path = os.path.join(IFTestBase.resource_directory, 'accused_head.csv')
-
-        with open(head_accused_path) as file:
-            actual_result = transformer.csv_to_entity_name_entity_list_tuples(file.read())
-
-            # print('***********')
-            print(actual_result[0][1])
-            # print('***********')
-
-    def test_csv_to_entity_name_entity_list_tuples_single(self):
-        transformer = AccusedTransformer()
-        head_accused_path = os.path.join(IFTestBase.resource_directory, 'accused_single_row.csv')
-
-        with open(head_accused_path) as file:
-
-            actual_result = transformer.csv_to_entity_name_entity_list_tuples(file.read())
-            expected_result = [
-                (
-                    'allegationcategory',
-                    AllegationCategory(
-                        category='0    EXCESSIVE FORCE / ON DUTY - INJURY\nName: ALLEGATION_CATEGORY, dtype: object',
-                        category_code='0    05A\nName: ALLEGATION_CATEGORY_CD, dtype: object',
-                        on_duty=False,
-                        cr_id='0    259794\nName: LOG_NO, dtype: int64')
-                ), (
-                    'officer',
-                    Officer(
-                        birth_year='0    1946\nName: BIRTH_YEAR, dtype: int64',
-                        first_name='0    EARL\nName: OFFICER_FIRST_NAME, dtype: object',
-                        gender='0    M\nName: SEX_CODE_CD, dtype: object',
-                        last_name='0    WASHINGTON\nName: OFFICER_LAST_NAME, dtype: object',
-                        middle_initial='0    B\nName: MIDDLE_INITIAL, dtype: object',
-                        race='0    BLK\nName: RACE_CODE_CD, dtype: object',
-                        rank='0    SERGEANT OF POLICE\nName: EMPLOYEE_POSITION, dtype: object',
-                        cr_id='0    259794\nName: LOG_NO, dtype: int64')
-                ), (
-                    'officerbadgenumber',
-                    OfficerBadgeNumber(
-                        officer_id='0    1088\nName: EMPLOYEE_NO, dtype: int64',
-                        star='0    1986\nName: STAR_NO, dtype: int64',
-                        cr_id='0    259794\nName: LOG_NO, dtype: int64')
-                ), (
-                    'policeunit',
-                    PoliceUnit(
-                        tags='0    3\nName: EMPLOYEE_DETAIL_UNIT, dtype: int64',
-                        unit_name='0   NaN\nName: EMPLOYEE_ASSIGN_UNIT, dtype: float64',
-                        cr_id='0    259794\nName: LOG_NO, dtype: int64')
-                )
-            ]
-            assert actual_result == expected_result
 
     def test_csv_to_entity_name_csv_tuples_head(self):
         transformer = AccusedTransformer()
