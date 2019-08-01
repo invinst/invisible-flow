80982a115949ce0dbbbcd4fd2b4108a9746c9684
Jeff Burroughs <50185149+tw-jeff-burroughs@users.noreply.github.com>
2019-08-01 11:17:13 -0500

    Accused transformer (#49)
    * Add entities for the 4 db tables
    
    * WIP: added single row transform from csv to list of name, tuple pairs
    
    * flake8 fixes
    
    * mypy fixes
    
    * add function to return table name, csv tuples for accused request type, add testing for new function
    
    * remove csv to entities function and tests, code cleanup
    
    * rename function to transform, update test, confirmed working with personal gcs instance
    
    * cleanup testing, uses csv files for transform tests
    
    * add data_ prefix to align with table names as per instructions in github issue 12
    
    * rename entity class file names to include underscores for readability

--- invisible_flow/transformers/__init__.py
+++ invisible_flow/transformers/__init__.py
@@ -1,5 +1,6 @@
 from invisible_flow.transformers.case_info_allegations_transformer import CaseInfoAllegationsTransformer
 from invisible_flow.transformers.investigator_transformer import InvestigatorTransformer
+from invisible_flow.transformers.accused_transformer import AccusedTransformer
 from invisible_flow.transformers.transformer_factory import TransformerFactory
 
-__all__ = ['CaseInfoAllegationsTransformer', 'InvestigatorTransformer', 'TransformerFactory']
+__all__ = ['AccusedTransformer', 'CaseInfoAllegationsTransformer', 'InvestigatorTransformer', 'TransformerFactory']
---
+++ invisible_flow/transformers/accused_transformer.py
@@ -0,0 +1,74 @@
+from typing import List, Tuple
+
+import pandas as pd
+from io import StringIO
+
+from invisible_flow.transformers.transformer_base import TransformerBase
+
+
+class AccusedTransformer(TransformerBase):
+
+    def transform(self, response_type: str, csv_content: str) -> List[Tuple[str, str]]:
+        string_io_csv = StringIO(csv_content)
+        accused_df = pd.read_csv(string_io_csv)
+        allegationcategory_columns = [
+            'ALLEGATION_CATEGORY',
+            'ALLEGATION_CATEGORY_CD',
+            'LOG_NO'
+        ]
+        officer_columns = [
+            'BIRTH_YEAR',
+            'OFFICER_FIRST_NAME',
+            'SEX_CODE_CD',
+            'OFFICER_LAST_NAME',
+            'MIDDLE_INITIAL',
+            'RACE_CODE_CD',
+            'EMPLOYEE_POSITION',
+            'LOG_NO'
+        ]
+        officerbadgenumber_columns = [
+            'EMPLOYEE_NO',
+            'STAR_NO',
+            'LOG_NO'
+        ]
+        policeunit_columns = [
+            'EMPLOYEE_DETAIL_UNIT',
+            'EMPLOYEE_ASSIGN_UNIT',
+            'LOG_NO'
+        ]
+        return [
+            (
+                'data_allegationcategory',
+                accused_df.loc[:, allegationcategory_columns].rename(columns={
+                    allegationcategory_columns[0]: 'category',
+                    allegationcategory_columns[1]: 'category_code',
+                    allegationcategory_columns[2]: 'cr_id'
+                }).to_csv(index=False)
+            ), (
+                'data_officer',
+                accused_df.loc[:, officer_columns].rename(columns={
+                    officer_columns[0]: 'birth_year',
+                    officer_columns[1]: 'first_name',
+                    officer_columns[2]: 'gender',
+                    officer_columns[3]: 'last_name',
+                    officer_columns[4]: 'middle_initial',
+                    officer_columns[5]: 'race',
+                    officer_columns[6]: 'rank',
+                    officer_columns[7]: 'cr_id'
+                }).to_csv(index=False)
+            ), (
+                'data_officerbadgenumber',
+                accused_df.loc[:, officerbadgenumber_columns].rename(columns={
+                    officerbadgenumber_columns[0]: 'officer_id',
+                    officerbadgenumber_columns[1]: 'star',
+                    officerbadgenumber_columns[2]: 'cr_id'
+                }).to_csv(index=False)
+            ), (
+                'data_policeunit',
+                accused_df.loc[:, policeunit_columns].rename(columns={
+                    policeunit_columns[0]: 'tags',
+                    policeunit_columns[1]: 'unit_name',
+                    policeunit_columns[2]: 'cr_id'
+                }).to_csv(index=False)
+            )
+        ]
