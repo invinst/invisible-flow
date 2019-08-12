95c4afff69c861ec0c3f68a93879bd5b289ea1f1
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-08-12 16:51:18 -0500

    initial commit for React CRA UI

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button data-testid='startScrape'>Start COPA scrape</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+describe("App goes to status", function () {
+  it('App should have scrape button', function () {
+    const { getByTestId } = render(<App />);
+    const e = getByTestId("startScrape");
+    expect(e.tagName).toBe("BUTTON");
+  });
+});
