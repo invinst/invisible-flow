b627ba315429a62eb0cfaee1b6617f4f1f26ae6a
Jeff Burroughs <50185149+tw-jeff-burroughs@users.noreply.github.com>
2019-08-13 09:55:38 -0700

    56 copa scrape react (#60)
    * initial commit for React CRA UI
    
    * left to do in test to guide us when we return
    
    * code cleanup, remove unneeded service workers and styles

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button data-testid='startScrape'>Start COPA scrape</button>
+    </div>
+  );
+}
+
+export default App;
