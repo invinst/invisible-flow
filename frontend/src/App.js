e895439b198d8e33ec1dbad4c3366cf7c3f022d8
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-08-20 14:46:49 -0500

    [56] Define startScrape to prevent breaking while rendering app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import StartScrapeButton from "./Components/StartScrapeButton";
 
 function App() {
+    // TODO actually call python api to start the scraping
+    // for now anonymous function will do until python api is ready
+    const startScrape = () => {};
       return (
         <div className="App">
           <h1>Invisible Flow</h1>
