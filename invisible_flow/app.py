4e3ad6e4d57e60865c2e0160ee7a9dbd173f6154
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-08-23 16:11:37 -0500

    fix flake8 issue

--- invisible_flow/app.py
+++ invisible_flow/app.py
@@ -52,7 +52,6 @@ def foia_response_upload():
         return Response(status=415, response='Unsupported file type. Please upload a .csv .xls or .xlsx file.')
 
     storage = StorageFactory.get_storage()
-    
     response_type = request_context.form['response_type']
     logger.info(f'Received foia request of type {response_type}')
 
