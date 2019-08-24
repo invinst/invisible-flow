b97bb6bc02d81fac92a1cf0ad640590dce2899ef
Adeolu Babatunde <adeolutechs@gmail.com>
2019-08-24 16:56:35 -0500

    removed debug statement

--- run-api-tests.py
+++ run-api-tests.py
@@ -8,14 +8,15 @@ import requests
 
 def wait_for_server_to_start():
     for i in range(5):
-        print(f'waited {i * 2} seconds for server...')
+        print(f'waited {i * 2} seconds for server to start...')
 
         try:
             r = requests.get('http://127.0.0.1:5000/status')
             if r.status_code == 200:
                 return
-        except Exception as e:
-            print(f'received exception {e}')
+        except Exception:
+            # Uncomment this for debug purposes
+            # print(f'received exception {e}')
             time.sleep(2)
     print("Server took too long to start")
     sys.exit(1)
@@ -32,7 +33,7 @@ server_process = subprocess.Popen(f'{python_executable} -m flask run', shell=Tru
 
 wait_for_server_to_start()
 
-# # Run the tests
+# # Run the testsxzl
 test_process = subprocess.run('pytest api_tests/', shell=True, capture_output=True, text=True)
 
 print('-----------------Test output')
