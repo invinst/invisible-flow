ebae009cd36472fc6aeb69aed68442238d46b2f6
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-08-27 16:42:21 -0500

    add test to verify can add JobRecord to db

--- tests/jobs/test_entities.py
+++ tests/jobs/test_entities.py
@@ -1,16 +1,14 @@
 import pytest
 
-from invisible_flow.app_factory import AppFactory
 from invisible_flow.constants import JOB_DB_BIND_KEY, VALID_STATUSES
 from invisible_flow.jobs.entities import JobRecord
-from manage import setup_db
+from manage import db
 
 
 class TestJobRecord:
 
     @pytest.fixture
-    def db(self):
-        db = setup_db(AppFactory.create_app())
+    def get_db(self):
         db.create_all(bind=JOB_DB_BIND_KEY)
 
         yield db
@@ -29,3 +27,9 @@ class TestJobRecord:
         expected_error_string = "The status attribute on Job Record can only receive one of: ['STARTED'," \
                                 " 'COMPLETED - SUCCESSFUL', 'COMPLETED - ERROR'], but received invalid status"
         assert expected_error_string == str(actual_error.value)
+
+    def test_adding_job_record_to_db_works(self, get_db):
+        for status in VALID_STATUSES:
+            get_db.session.add(JobRecord(status=status))
+        get_db.session.commit()
+        assert len(JobRecord.query.all()) == len(VALID_STATUSES)
