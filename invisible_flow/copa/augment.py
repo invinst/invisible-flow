40a706f0b17e341c6bb19fea666f27e827f9258c
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-08-30 17:02:36 -0500

    [#21] WIP: Augment transformed copa data, handles no categories in table, needs better testing

--- invisible_flow/copa/augment.py
+++ invisible_flow/copa/augment.py
@@ -1,6 +1,27 @@
 import pandas as pd
+from invisible_flow.copa.data_allegation_category import AllegationCategory
+from invisible_flow.constants import COPA_DB_BIND_KEY
+
+from manage import db
 
 
 class Augment:
-    def get_transform_copa_data(self, allegation_rows: str):
-        print(pd.read_csv(allegation_rows))
+    def get_augmented_copa_data(self, allegation_rows: str):
+        df = pd.read_csv(allegation_rows)
+        db.create_all(bind=COPA_DB_BIND_KEY)
+
+        categories = df.loc[:, 'current_category'].unique()
+
+        category_code_map = pd.DataFrame(
+            AllegationCategory.query.with_entities(AllegationCategory.category, AllegationCategory.category_code)
+        )
+
+        if len(category_code_map) > 0:
+            for category in categories:
+                # civil_suits_code is the category_code from data_allegationcategories
+                # table that corresponds to the current category in this loop
+                civil_suits_code = \
+                    category_code_map.loc[category_code_map['category'] == category]['category_code'].values[0]
+                df = df.replace(category, civil_suits_code)
+
+        return df
--- tests/copa/test_augment.py
+++ tests/copa/test_augment.py
@@ -5,6 +5,23 @@ from tests.helpers.if_test_base import IFTestBase
 
 class TestAugment:
 
-    def test_adding_copa_record_to_db_works(self):
+    def test_adding_augmented_copa_record_to_db(self):
+        # using test file that is not actual copa that has been cleaned/transformed
         copa_split_csv = os.path.join(IFTestBase.resource_directory, 'copa_scraped_split.csv')
-        Augment().get_transform_copa_data(copa_split_csv)
+
+        # TODO fix up tests, verify that with a lits of AllegationCategories that has at least
+        # one category/category_code pair that exists in our current csv
+        # verify dataframe returned has replaced those values correctly
+
+        # for experiments and testing only
+        # for i in range(len(categories)):
+        #     db.session.add(AllegationCategory(
+        #         category=categories[i],
+        #         category_code=i,
+        #         cr_id='cats meow' + str(i)
+        #     ))
+        #     db.session.commit()
+        # for experiments and testing only
+        Augment().get_augmented_copa_data(copa_split_csv)
+
+    # TODO add test for when no AllegationCategories match categories in list
