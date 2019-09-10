15832825d5375e4e7cecb3aa3ee8e775bcbee505
Jeffery Burroughs <jeff.burroughs@thoughtworks.com>
2019-09-10 16:18:41 -0500

    [#21] WIP: if partial matches upload file to gcs that has information about row collisions

--- invisible_flow/copa/loader.py
+++ invisible_flow/copa/loader.py
@@ -2,6 +2,8 @@ import numpy
 import pandas as pd
 
 from invisible_flow.copa.data_allegation import Allegation
+from invisible_flow.globals_factory import GlobalsFactory
+from invisible_flow.storage.storage_factory import StorageFactory
 from manage import db
 
 
@@ -12,8 +14,11 @@ class Loader:
             Allegation.query.with_entities(Allegation.cr_id)
         ).values.flatten()
         self.partial_matches = []
+        self.storage = StorageFactory.get_storage()
+        self.current_date = GlobalsFactory.get_current_datetime_utc().isoformat(sep='_').replace(':', '-')
 
     def load_copa_db(self, augmented_data: pd.DataFrame):
+        copa_column_names = ["cr_id", "beat_id", "incident_date"]
         for row in augmented_data.iterrows():
             cr = Allegation(
                 cr_id=row[1]["log_no"],
@@ -26,13 +31,26 @@ class Loader:
                 numpy.append(self.db_values, row[1]["log_no"])
             else:
                 db_row_match_log_no = Allegation.query.filter_by(cr_id=row[1]["log_no"]).all()[0]
-                if cr.beat_id != db_row_match_log_no.beat_id or cr.incident_date != db_row_match_log_no.beat_id:
+                if cr.beat_id != db_row_match_log_no.beat_id or cr.incident_date != db_row_match_log_no.incident_date:
                     self.partial_matches.append(cr)
-        return {
-            'partial_matches': self.partial_matches
-        }
 
-    # TODO convert array of Allegation objects to changed_allegation.csv
-    #  upload changed_allegation.csv to gcp under /errors
-    #  handle partial matches where db row is missing data populated in augmented row
+        allegation_rows = Allegation.query.all()
+        df = pd.DataFrame(
+            [(row.cr_id, row.beat_id, row.incident_date) for row in allegation_rows],
+            columns=copa_column_names
+        )
+
+        if len(df) > 0:
+            self.storage.store_string(
+                'changed_allegation.csv',
+                df.to_csv(index=False),
+                f'Scrape-{self.current_date}/errors'
+            )
+            return {
+                'partial_matches': self.partial_matches
+            }
+
+        return {}
+
+    # TODO handle partial matches where db row is missing data that is populated in augmented row
     #  this should update the db row and also put out a file under errors/updated_allegations.csv
--- tests/copa/test_loader.py
+++ tests/copa/test_loader.py
@@ -1,15 +1,30 @@
 import os
+from datetime import datetime
+from unittest import mock
+
+import pytest
+from mock import patch
 
 from invisible_flow.copa.augment import Augment
 from invisible_flow.copa.data_allegation import Allegation
 from invisible_flow.constants import COPA_DB_BIND_KEY
 from invisible_flow.copa.loader import Loader
+from invisible_flow.storage import LocalStorage
 
 from manage import db
 from tests.helpers.if_test_base import IFTestBase
 
 
+@patch('invisible_flow.app.GlobalsFactory.get_current_datetime_utc', lambda: datetime(2019, 3, 25, 5, 30, 50, 0))
 class TestLoad:
+    # TODO mock StorageFactory.get_storage
+    #  mock GlobalsFactory.get_current_datetime_utc
+
+    @pytest.fixture(autouse=True)
+    def default_fixture(self):
+        with patch('invisible_flow.app.StorageFactory.get_storage') as get_storage_mock:
+            get_storage_mock.return_value = LocalStorage()
+            yield get_storage_mock
 
     def test_load_augmented_db(self):
         db.drop_all()
@@ -29,7 +44,7 @@ class TestLoad:
         Loader().load_copa_db(aug_copa_data)
         assert length_of_allegation == len(Allegation.query.all())
 
-    def test_where_augmented_data_is_partial_match(self):
+    def test_where_augmented_data_is_partial_match(self, default_fixture):
         db.drop_all()
         db.create_all(bind=COPA_DB_BIND_KEY)
         copa_split_csv = os.path.join(IFTestBase.resource_directory, 'copa_scraped_split.csv')
@@ -37,5 +52,7 @@ class TestLoad:
         aug_copa_data = Augment().get_augmented_copa_data(copa_split_csv)
         aug_mod_data = Augment().get_augmented_copa_data(copa_modded)
         Loader().load_copa_db(aug_copa_data)
-        partial_matches = Loader().load_copa_db(aug_mod_data)
-        assert len(aug_mod_data) == len(partial_matches["partial_matches"])
+        with patch.object(LocalStorage, 'store_string') as store_string_mock:
+            partial_matches = Loader().load_copa_db(aug_mod_data)
+            assert len(aug_mod_data) == len(partial_matches["partial_matches"])
+            store_string_mock.assert_called_with('changed_allegation.csv', mock.ANY, mock.ANY)
