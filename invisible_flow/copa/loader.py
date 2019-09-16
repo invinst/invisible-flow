f92a2607b09312d94ab353ec9840f1caca2af19a
Jeff Burroughs <50185149+tw-jeff-burroughs@users.noreply.github.com>
2019-09-16 16:29:35 -0500

    [#56] WIP: add rows_added to db property to loader for use in UI (#75)

--- invisible_flow/copa/loader.py
+++ invisible_flow/copa/loader.py
@@ -9,11 +9,13 @@ from manage import db
 class Loader:
 
     def __init__(self):
-        self.db_values = pd.DataFrame(
+        self.log_nos = pd.DataFrame(
             Allegation.query.with_entities(Allegation.cr_id)
         ).values.flatten().tolist()
         self.partial_matches = []
+        self.db_rows_added = []
         self.error_notes = pd.DataFrame()
+        self.changed_allegations = pd.DataFrame()
         self.storage = StorageFactory.get_storage()
         self.current_date = GlobalsFactory.get_current_datetime_utc().isoformat(sep='_').replace(':', '-')
 
@@ -26,46 +28,48 @@ class Loader:
     def load_copa_db(self, augmented_data: pd.DataFrame):
         copa_column_names = ["cr_id", "beat_id", "incident_date"]
         for row in augmented_data.iterrows():
-            cr = Allegation(
-                cr_id=str(row[1]["log_no"]),
+            log_no = row[1]["log_no"]
+            current_allegation = Allegation(
+                cr_id=str(log_no),
                 beat_id=str(row[1]["beat"]),
                 incident_date=str(row[1]["complaint_date"])
             )
-            if row[1]["log_no"] not in self.db_values:
-                db.session.add(cr)
+            if log_no not in self.log_nos:
+                db.session.add(current_allegation)
                 db.session.commit()
-                self.db_values.append(row[1]["log_no"])
+                self.log_nos.append(log_no)
+                self.db_rows_added.append(current_allegation)
             else:
-                db_row_match_log_no = Allegation.query.filter_by(cr_id=row[1]["log_no"]).all()[0]
+                db_row_match_log_no = Allegation.query.filter_by(cr_id=log_no).all()[0]
                 had_partial_match = False
                 for column_name in Allegation.__table__.columns.keys():
-                    if cr[column_name] != db_row_match_log_no[column_name]:
+                    if current_allegation[column_name] != db_row_match_log_no[column_name]:
                         had_partial_match = True
                         self.error_notes = self.error_notes.append(
                             {
                                 "error_notes":
                                     self.get_error_note(
                                          column_name,
-                                         cr.cr_id,
+                                         current_allegation.cr_id,
                                          db_row_match_log_no[column_name],
-                                         cr[column_name]
+                                         current_allegation[column_name]
                                     )
                             },
                             ignore_index=True
                         )
 
                 if had_partial_match:
-                    self.partial_matches.append(cr)
+                    self.partial_matches.append(current_allegation)
 
-        df = pd.DataFrame(
+        self.changed_allegations = pd.DataFrame(
             [(row.cr_id, row.beat_id, row.incident_date) for row in self.partial_matches],
             columns=copa_column_names
         )
 
-        if len(df) > 0:
+        if len(self.changed_allegations) > 0:
             self.storage.store_string(
                 'changed_allegation.csv',
-                df.to_csv(index=False),
+                self.changed_allegations.to_csv(index=False),
                 f'Scrape-{self.current_date}/load_errors'
             )
             self.storage.store_string(
@@ -73,11 +77,6 @@ class Loader:
                 self.error_notes.to_csv(index=False),
                 f'Scrape-{self.current_date}/load_errors'
             )
-            return {
-                'partial_matches': self.partial_matches
-            }
-
-        return {}
 
     # TODO handle partial matches where db row is missing data that is populated in augmented row
     #  this should update the db row and also put out a file under errors/updated_allegations.csv
