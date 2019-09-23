f0a7f8528f12806df7b8cb4050dd76bc06f798f5
Colin Parsons <colin.parsons@thoughtworks.com>
2019-09-23 16:23:38 -0500

    [#56] WIP: Added test file for copa_scrape, called test_copa_scrape

--- invisible_flow/api/copa_scrape.py
+++ invisible_flow/api/copa_scrape.py
@@ -4,6 +4,7 @@ from invisible_flow.constants import SCRAPE_URL
 
 
 class CopaScrape:
+
     def scrape_data_json(self):
         url = SCRAPE_URL + ".json"
         resp = requests.get(url=url)
@@ -36,5 +37,4 @@ class CopaScrape:
     def scrape_data_csv(self):
         url = SCRAPE_URL + ".csv"
         resp = requests.get(url=url)
-        print("*******************", type(resp.content))
         return resp.content
---
+++ tests/api/test_copa_scrape.py
@@ -0,0 +1,63 @@
+import unittest
+
+from unittest import mock
+
+from invisible_flow.api import CopaScrape
+from invisible_flow.constants import SCRAPE_URL
+
+
+def mocked_requests_get(**kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code, content):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.content = content
+
+        def json(self):
+            return self.json_data
+    if kwargs == {"url": SCRAPE_URL + ".csv"}:
+        return MockResponse({"key1": "value1"}, 200, "bubbles")
+    elif kwargs == {"url": SCRAPE_URL + ".json"}:
+        return MockResponse({"key1": "value1"}, 200, "bubbles")
+    elif kwargs == {"url": SCRAPE_URL + ".csv?$where=assignment=\"COPA\""}:
+        return MockResponse({"key1": "value1"}, 200, "bubbles")
+    elif kwargs == {"url": SCRAPE_URL + ".csv?$where=assignment!=\"COPA\""}:
+        return MockResponse({"key1": "value1"}, 200, "bubbles")
+    elif kwargs == {"url": SCRAPE_URL + ".csv?$select=log_no,complaint_date,beat&$where=assignment!=\"COPA\""}:
+        return MockResponse({"key1": "value1"}, 200, "bubbles")
+    elif kwargs == {"url": SCRAPE_URL + ".csv?$select=log_no,assignment,case_type,current_status,current_category,"
+                    "finding_code,police_shooting,race_of_complainants,sex_of_complainants,age_of_complainants,"
+                    "race_of_involved_officers,sex_of_involved_officers,age_of_involved_officers,"
+                    "years_on_force_of_officers,complaint_hour,complaint_day,complaint_month&"
+                    "$where=assignment!=\"COPA\""}:
+        return MockResponse({"key1": "value1"}, 200, "bubbles")
+
+    return MockResponse(None, 404, None)
+
+
+@mock.patch('requests.get', side_effect=mocked_requests_get)
+class TestCopaScrape(unittest.TestCase):
+
+    def test_scrape_data_csv(self, get_mock):
+        should_be_bubbles = CopaScrape().scrape_data_csv()
+        assert should_be_bubbles == "bubbles"
+
+    def test_scrape_data_json(self, get_mock):
+        should_be_json_data = CopaScrape().scrape_data_json()
+        assert should_be_json_data == {"key1": "value1"}
+
+    def test_scrape_copa_csv(self, get_mock):
+        should_be_bubbles = CopaScrape().scrape_copa_csv()
+        assert should_be_bubbles == "bubbles"
+
+    def test_scrape_not_copa_csv(self, get_mock):
+        should_be_bubbles = CopaScrape().scrape_not_copa_csv()
+        assert should_be_bubbles == "bubbles"
+
+    def test_scrape_copa_ready_for_entity(self, get_mock):
+        should_be_bubbles = CopaScrape().scrape_copa_ready_for_entity()
+        assert should_be_bubbles == "bubbles"
+
+    def test_scrape_copa_not_in_entity(self, get_mock):
+        should_be_bubbles = CopaScrape().scrape_copa_not_in_entity()
+        assert should_be_bubbles == "bubbles"
