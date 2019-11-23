31e31367c131941e9d0b9ea1b3e63f8d45e4b0a9
j0vanka <jovanka.placeres@thoughtworks.com>
2019-11-23 02:20:28 -0600

    [#56A][JK] WIP: bg image fix; styling

--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import StartScrapeButton from './StartScrapeButton'
-import bgPic from '../images/AllTheDocs.jpeg'
 
 
 function MainPage({startScrape}){
   return(
-    <div className="MainPage">
-        <img src={bgPic}  alt=''/>
+    <div className="MainPage bgImg">
         <StartScrapeButton startScrape={startScrape} />
     </div>
   );
