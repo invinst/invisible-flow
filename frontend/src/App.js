79848762b4597e4e91f304c184640d02b08fccc1
j0vanka <53668872+j0vanka@users.noreply.github.com>
2019-11-27 02:14:36 -0600

    link/routes fix (#82)
    * [#56A][Jylia]fix/update: menu routing; moved Header component into Router component, nesting within BrowserRouter satisfies test error and routing of pages; delete routes.jsx, not needed; update styling on bgImg, 'fixed' removed; site title routes to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header';
 import MainPage from './Components/MainPage';
 import Footer from './Components/Footer';
 
@@ -10,7 +9,6 @@ function App() {
     const startScrape = () => {};
       return (
         <div className="App">
-          <Header />
           <MainPage startScrape={startScrape}/>
           <Footer />
         </div>
--- frontend/src/Components/ScrapeStatus.js
+++ frontend/src/Components/ScrapeStatus.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from './Header';
 
 function ScrapeStatus() {
     return (
         <div>
-           <Header />
            <h1>COPA Scraping Status</h1>
         </div>
     );
