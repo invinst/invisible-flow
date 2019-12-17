ecfe7ac691e66723b4a961c8528ffc3fae83d194
j0vanka <jovanka.placeres@thoughtworks.com>
2019-12-17 13:20:11 -0600

    [#41][OVJ] update storageFactory file; heroku hook up testing

--- invisible_flow/storage/storage_factory.py
+++ invisible_flow/storage/storage_factory.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 from google.cloud import storage
 
 from invisible_flow.storage import GCStorage, LocalStorage
@@ -14,6 +14,12 @@ class StorageFactory:
             return LocalStorage()
         elif os.environ.get('ENVIRONMENT') == 'gae':
             gcs_client = storage.Client()
+            return GCStorage(gcs_client)
+        elif os.environ.get('ENVIRONMENT') == 'heroku':
+            creds = os.environ.get('TEMP')
+            with open('googleCred.json', 'w') as f:  # writing JSON object
+                json.dump(creds, f)
+            gcs_client = storage.Client.from_service_account_json('googleCred.json')
             return GCStorage(gcs_client)
         else:
             raise Exception('Unable to determine the environment')
