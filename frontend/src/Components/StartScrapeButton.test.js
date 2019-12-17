00c539adf68ed2381030b67ac2a530295ed1420a
Owen Nelson <owen.nelson@thoughtworks.com>
2019-12-17 11:28:28 -0600

    [#33][Owen] implemented testing library utility functions in StartScrapeButton test

--- frontend/src/Components/StartScrapeButton.test.js
+++ frontend/src/Components/StartScrapeButton.test.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { render, fireEvent } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import StartScrapeButton from './StartScrapeButton';
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<StartScrapeButton />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<StartScrapeButton />);
 });
 
 describe("When StartScrapeButton is clicked", () => {
-    it("redirects to scrape status page", () => {
+  it("redirects to scrape status page", () => {
     const windowLocationMock = window.location.assign = jest.fn();
-    const { getByTestId } = render(<StartScrapeButton/>);
-    fireEvent.click(getByTestId("startScrape"));
+    const { getByText } = render(<StartScrapeButton />);
+
+    fireEvent.click(getByText("Initiate COPA Scrape"))
+
     expect(windowLocationMock).toBeCalledWith("/scrapeStatus");
   });
 });
