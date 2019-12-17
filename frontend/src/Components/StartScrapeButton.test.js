453b221c7472420cb29f8e84ad8c158fd0595d91
Owen Nelson <owen.nelson@thoughtworks.com>
2019-12-16 18:33:39 -0600

    [#33][Owen] rewords StartScrapeButton test for clarity and removes duplicate test

--- frontend/src/Components/StartScrapeButton.test.js
+++ frontend/src/Components/StartScrapeButton.test.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom';
 import { render, fireEvent } from "@testing-library/react";
 import StartScrapeButton from './StartScrapeButton';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<StartScrapeButton />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-describe("StartScrapeButton goes to status", function () {
-  it('StartScrapeButton should have scrape button', function () {
-    const { getByTestId } = render(<StartScrapeButton />);
-    const e = getByTestId("startScrape");
-    expect(e.tagName).toBe("BUTTON");
-  });
-
-  it('scrape button should redirect to scrape status page', function () {
-    // test to show that the user hit scrape page
+describe("When StartScrapeButton is clicked", () => {
+    it("redirects to scrape status page", () => {
     const windowLocationMock = window.location.assign = jest.fn();
     const { getByTestId } = render(<StartScrapeButton/>);
     fireEvent.click(getByTestId("startScrape"));
