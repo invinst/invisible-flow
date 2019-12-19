5addb11ebf533c852425353c5ae0559f0e28cc30
Owen Nelson <owen.nelson@thoughtworks.com>
2019-12-19 10:27:26 -0600

    [#33][Owen] Cleaning and adding setup/teardown functions to StartScrapeButtonTest

--- frontend/src/Components/StartScrapeButton.js
+++ frontend/src/Components/StartScrapeButton.js
@@ -3,16 +3,18 @@ import React from 'react';
 function StartScrapeButton() {
   function goToScrapeStatus() {
     const request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:5000/copa_scrape", false)
-    request.send()
+    request.open("GET", "http://localhost:5000/copa_scrape", false);
+    request.send();
     if (request.status === 200) {
-      window.location.assign("/scrapeStatus")
+      window.location.assign("/scrapeStatus");
     }
   }
 
-  return <button className='Scrape-Button' onClick={goToScrapeStatus} data-testid='startScrape'>
-           Initiate COPA Scrape
-         </button>
+  return (
+    <button className='Scrape-Button' onClick={goToScrapeStatus} data-testid='startScrape'>
+      Initiate COPA Scrape
+    </button>
+  );
 }
 
 export default StartScrapeButton;
--- frontend/src/Components/StartScrapeButton.test.js
+++ frontend/src/Components/StartScrapeButton.test.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import { unmountComponentAtNode } from "react-dom";
 import { fireEvent, render } from "@testing-library/react";
 import StartScrapeButton from './StartScrapeButton';
 
-const mockXHR = {
+let container = null;
+const windowLocationMock = window.location.assign = jest.fn();
+window.XMLHttpRequest = jest.fn(() => mockRequest);
+
+const mockRequest = {
   open: jest.fn(),
   send: jest.fn(),
   status: 200
 };
 
-const windowLocationMock = window.location.assign = jest.fn();
-window.XMLHttpRequest = jest.fn(() => mockXHR);
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 it("renders without crashing", () => {
-  render(<StartScrapeButton />);
+  render(<StartScrapeButton />, container);
 });
 
 describe("When StartScrapeButton is clicked", () => {
   it("redirects to scrape status page and sends HTTP request to backend", () => {
-    const { getByText } = render(<StartScrapeButton />);
+    const { getByText } = render(<StartScrapeButton />, container);
 
-    fireEvent.click(getByText("Initiate COPA Scrape"))
+    fireEvent.click(getByText("Initiate COPA Scrape"));
 
     expect(windowLocationMock).toBeCalledWith("/scrapeStatus");
-    expect(mockXHR.send).toHaveBeenCalled();
+    expect(mockRequest.send).toHaveBeenCalled();
   });
 });
