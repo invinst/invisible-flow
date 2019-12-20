e8c5bdf41b170e39ba9d6a46d11b243b2c76cf87
Omar Carey <omar.carey@thoughtworks.com>
2019-12-20 10:46:55 -0600

    [#33] Kha/Omar fixes to foiauploadform test regarding async

--- frontend/src/Components/FOIAUploadForm.test.js
+++ frontend/src/Components/FOIAUploadForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { unmountComponentAtNode } from 'react-dom';
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect'
 import { getNodeText, getAllByText, getByTestId } from '@testing-library/dom';
 import FOIAUploadForm from './FOIAUploadForm';
@@ -33,11 +33,12 @@ describe("When FOIAUploadForm is submitted", function () {
           return p;
         });
 
-      const { getAllByText, getByTestId} = render(<FOIAUploadForm/>);
+      const { getAllByText, getByTestId, container} = render(<FOIAUploadForm/>);
       const button = getAllByText("Upload")[0];
       fireEvent.click(button);
-      const banner = await getByTestId("resultsBanner");
-      //console.log(getNodeText(banner));
+      expect(fetch).toHaveBeenCalled();
+
+      const banner = await waitForElement(() => getByTestId("resultsBanner"), {container})
       expect(banner).toHaveTextContent("Your file has been successfully uploaded.");
     });
 
@@ -56,8 +57,9 @@ describe("When FOIAUploadForm is submitted", function () {
       const { getAllByText, getByTestId} = render(<FOIAUploadForm/>);
       const button = getAllByText("Upload")[0];
       fireEvent.click(button);
-      const banner = await getByTestId("resultsBanner");
-      //console.log(getNodeText(banner));
+      expect(fetch).toHaveBeenCalled();
+
+      const banner = await waitForElement(() => getByTestId("resultsBanner"), {container})
       expect(banner).toHaveTextContent("Error: UNSUPPORTED MEDIA TYPE. Please resolve the issue and try again.");
     });
 
