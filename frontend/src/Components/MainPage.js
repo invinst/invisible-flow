9bb5fc813fe88af4292b1c20473003fd5a5afaf1
khathuynh-tw <kha.huynh@thoughtworks.com>
2020-01-15 16:04:03 -0600

    #75 [Kha/Kyle] playground/temp code for toggling between loading and ready states.. in progresss

--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import StartScrapeButton from './StartScrapeButton';
 
-function MainPage(){
-  return(
-    <div className='MainPage'>
-      <StartScrapeButton />
-    </div>
-  );
+class MainPage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        isScraping: false
+    };
+    this.toggleLoading = this.toggleLoading.bind(this);
+  }
+
+  toggleLoading() {
+    console.log('child event on parent');
+    console.log('before: ' + this.state.isScraping);
+    this.setState({isScraping: !this.state.isScraping}, () => {
+        console.log(this.state.isScraping);
+    });
+  }
+
+  render() {
+    if(this.state.isScraping){
+        return(
+            <div className='MainPage'>
+              <h1>LOADING</h1>
+            </div>
+        );
+
+    } else{
+        return(
+        <div className='MainPage'>
+          <StartScrapeButton toggleLoading={this.toggleLoading}/>
+        </div>
+      );
+    }
+  }
+
 }
 
 export default MainPage;
