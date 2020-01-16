763c47b861aa697ba16cc0593ee6a7f0098f9027
Kyle Dolezal <kyle.dolezal@thoughtworks.com>
2020-01-15 19:19:59 -0600

    #75 [Kha/Kyle] Use await/async to ensure state has been updated before initiating scrape... still in progresss

---
+++ frontend/src/Components/HomepageOverlay.js
@@ -0,0 +1,19 @@
+import React from 'react;
+import MainPage from './MainPage';
+import Header from './Header';
+import Footer from './Footer';
+
+//
+//import React from 'react';
+//import './App.css';
+//import MainPage from './Components/MainPage';
+//
+//function App() {
+//  return (
+//    <div className='App'>
+//      <MainPage />
+//    </div>
+//  );
+//}
+//
+//export default App;
