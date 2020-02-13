2b406e792b05573bae2aee25e1d535bf4fed4327
j0vanka <jovanka.placeres@thoughtworks.com>
2020-02-13 12:20:46 -0600

    [#78][KyJo] add scripts for testing & staging environments; cleans/inserts rows

---
+++ reset-db-testing.sh
@@ -0,0 +1,17 @@
+ECHO -- Preparing to clean database --
+
+partA="postgresql-"
+partB=$(heroku pg:info --app invisiflow-testing | grep postgresql | cut -d "-" -f 3)
+partC=$(heroku pg:info --app invisiflow-testing | grep postgresql | cut -d "-" -f 4)
+# echo "$partA$partB-$partC"
+
+ECHO "DELETE FROM data_allegation;" | heroku pg:psql $partA$partB-$partC --app invisiflow-testing
+
+ECHO -- Inserting data into database --
+ECHO "INSERT INTO data_allegation(cr_id, summary, add1, add2, city, is_officer_complaint, location, subjects, created_at, updated_at) VALUES('C227980', '', '', '', '', false, '', '{}',  '2019-01-09 12:41:26.530261+08', '2019-01-09 12:41:31.205347+08');" | heroku pg:psql $partA$partB-$partC --app invisiflow-testing
+ECHO "INSERT INTO data_allegation(cr_id, summary, add1, add2, city, is_officer_complaint, location, subjects, created_at, updated_at) VALUES('1001892', '', '', '?', 'CHICAGO ILLINOIS', false, '', '{}',  '2019-01-09 12:41:26.530261+08', '2019-01-09 12:41:31.205347+08');" | heroku pg:psql $partA$partB-$partC --app invisiflow-testing
+ECHO "INSERT INTO data_allegation(cr_id, summary, add1, add2, city, is_officer_complaint, location, subjects, created_at, updated_at) VALUES('1002393', '', '60XX', 'South JUSTINE ST', 'CHICAGO ILLINOIS 60636', false, 'street', '{}',  '2019-01-09 12:41:26.530261+08', '2019-01-09 12:41:31.205347+08');" | heroku pg:psql $partA$partB-$partC --app invisiflow-testing
+ECHO "INSERT INTO data_allegation(cr_id, summary, add1, add2, city, is_officer_complaint, location, subjects, created_at, updated_at) VALUES('1003121', '', '', '?', 'CHICAGO ILLINOIS ', false, 'street', '{}',  '2019-01-09 12:41:26.530261+08', '2019-01-09 12:41:31.205347+08');" | heroku pg:psql $partA$partB-$partC --app invisiflow-testing
+ECHO "INSERT INTO data_allegation(cr_id, summary, add1, add2, city, is_officer_complaint, location, subjects, created_at, updated_at) VALUES('C228609', '', '', '', '', false, '', '{}',  '2019-01-09 12:41:26.530261+08', '2019-01-09 12:41:31.205347+08');" | heroku pg:psql $partA$partB-$partC --app invisiflow-testing
+
+ECHO -- Database loaded --
