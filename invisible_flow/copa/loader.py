9a89c8003557fce09de27eb95558f57bb4af41cd
Kyle Dolezal <kyle.dolezal@thoughtworks.com>
2020-02-20 09:16:21 -0600

    [#84][Kyle] Try to insert every data allegation, catching and recording duplicates as they occur; remove full-table query previously used to identify duplicates

--- invisible_flow/copa/loader.py
+++ invisible_flow/copa/loader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sqlalchemy.exc import IntegrityError
 
 from manage import db
 from invisible_flow.copa.data_allegation import DataAllegation
@@ -7,9 +8,6 @@ from invisible_flow.copa.data_allegation import DataAllegation
 class Loader:
 
     def __init__(self):
-        self.existing_crid = pd.DataFrame(
-            DataAllegation.query.with_entities(DataAllegation.cr_id)
-        ).values.flatten().tolist()
         self.matches = []
         self.new_data = []
 
@@ -17,14 +15,16 @@ class Loader:
 
         for row in transformed_data.itertuples():
 
-            if row.cr_id not in self.existing_crid:
-                new_allegation = DataAllegation(cr_id=row.cr_id)
-                db.session.add(new_allegation)
-                self.new_data.append(transformed_data.iloc[row[0]])
-            else:
+            new_allegation = DataAllegation(cr_id=row.cr_id)
+            db.session.add(new_allegation)
+            try:
+                db.session.commit()
+            except IntegrityError:
                 self.matches.append(transformed_data.iloc[row[0]])
+                db.session.rollback()
+            else:
+                self.new_data.append(transformed_data.iloc[row[0]])
 
-        db.session.commit()
         db.session.close()
 
     def get_matches(self):
