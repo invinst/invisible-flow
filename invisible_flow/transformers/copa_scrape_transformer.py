71f1156baa45aeafda218d77e45c8ed19f87e46f
Octavia Shaw <shawoj93@gmail.com>
2020-02-25 16:28:31 -0600

    [#81] [Kyle/Octavia] Add number of rows to pd data frame; integration test still failing

--- invisible_flow/transformers/copa_scrape_transformer.py
+++ invisible_flow/transformers/copa_scrape_transformer.py
@@ -12,20 +12,20 @@ class CopaScrapeTransformer:
     def transform(self, scraped_data: bytes):
         self.initial_data = pd.read_csv(BytesIO(scraped_data), encoding='utf-8', sep=",", dtype=str)
 
-        crid = self.transform_logno_to_crid()
-        # number_rows = self.transform_officer_demographics_to_number_of_rows()
+        crid = self.__transform_logno_to_crid()
+        number_rows = self.__transform_officer_demographics_to_number_of_rows()
+
         self.transformed_data.insert(0, "cr_id", crid)
+        self.transformed_data.insert(1, "number_of_officer_rows", number_rows)
 
-    def transform_logno_to_crid(self):
-        #  transformed_logno = self.initial_data["log_no"].transform(lambda logno: logno)
-        return self.initial_data["log_no"]
+    def __transform_logno_to_crid(self):
+        transformed_logno = self.initial_data["log_no"].transform(lambda logno: logno)
 
-    def transform_officer_demographics_to_number_of_rows(self):
-        # number_of_rows = self.intial_data["sex_of_involved_officers"].transform(lambda sex_of_involved_officers:
-        #                                                                       sex_of_involved_officers.split('|')
-        #                                                                        .length())
+        return transformed_logno
 
-        number_of_rows = 3
+    def __transform_officer_demographics_to_number_of_rows(self):
+        number_of_rows = self.initial_data["sex_of_involved_officers"].transform(lambda sex: 0 if pd.isnull(sex) else len(sex.split('|')))
+
         return number_of_rows
 
     def get_transformed_data(self):
