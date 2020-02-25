59da0a09c833afaf170e1ca80b21a678e525dedb
Octavia Shaw <shawoj93@gmail.com>
2020-02-24 18:16:33 -0600

    [#81][Kyle/Octavia] Update branches

--- tests/helpers/testing_data.py
+++ tests/helpers/testing_data.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 transformed_data = pd.DataFrame(
         {
-            'cr_id': ["1008899", "1087378", "1087387", "1087308", "1008913"]
+            'cr_id': ["1008899", "1087378", "1087387", "1087308", "1008913"],
+        }
+    )
+
+
+transformed_data_with_rows = pd.DataFrame(
+        {
+            'cr_id': ["1008899", "1087378", "1087387", "1087308", "1008913"],
+            'number_of_officer_rows': [0, 0, 3, 2, 0]
         }
     )
