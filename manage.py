2ce58487bcb552867a528019b5718a7f073c85a1
Owen Nelson <owen.nelson@thoughtworks.com>
2020-02-26 14:47:33 -0600

    [#76][Owen] remove exception for no database environment variable

--- manage.py
+++ manage.py
@@ -12,14 +12,12 @@ job_db_file = tempfile.NamedTemporaryFile(suffix='.db')
 job_db_filename = f'sqlite:///{job_db_file.name}'
 
 copa_db_filename: Optional[str] = ''
-if os.environ.get("ENVIRONMENT") == 'local' or os.environ.get('ENVIRONMENT') == 'travis':
-    copa_db_filename = 'postgres+psycopg2://invisible_flow@localhost:5432/invisible_flow_testing'
-elif os.environ.get('ENVIRONMENT') == 'heroku':
+if os.environ.get('ENVIRONMENT') == 'heroku':
     heroku_db_url = str(os.environ.get('DATABASE_URL'))
     split_heroku_db_url = heroku_db_url.split(':', 1)
     copa_db_filename = split_heroku_db_url[0] + '+psycopg2:' + split_heroku_db_url[1]
 else:
-    raise Exception('Unable to determine environment when setting database URL')
+    copa_db_filename = 'postgres+psycopg2://invisible_flow@localhost:5432/invisible_flow_testing'
 
 
 def setup_db(_app: Flask) -> SQLAlchemy:
@@ -28,7 +26,6 @@ def setup_db(_app: Flask) -> SQLAlchemy:
             JOB_DB_BIND_KEY: job_db_filename,
             COPA_DB_BIND_KEY: copa_db_filename
         },
-        # 'SQLALCHEMY_DATABASE_URI':        <-- WAS IST DASS ADEOLU?
     }
 
     _app.config.update(db_config)
