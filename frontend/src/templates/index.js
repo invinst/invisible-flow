fc4313b0888eb9e3a27d7ba1e601c1e90e951e82
Kyle Dolezal <kyle.dolezal@thoughtworks.com>
2020-03-16 23:25:10 -0500

    asdf

--- frontend/src/templates/index.js
+++ frontend/src/templates/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import '../style.css';
 import Router from './Components/Router';
 
 ReactDOM.render( <Router />, document.getElementById('root'));
