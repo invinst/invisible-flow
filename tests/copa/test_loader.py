c9aaa697a5e84e2e4e7a2f0339c993cf85216460
j0vanka <jovanka.placeres@thoughtworks.com>
2020-05-16 16:46:22 -0500

    [76] clean up; unused code/comments

--- tests/copa/test_loader.py
+++ tests/copa/test_loader.py
@@ -1,22 +1,14 @@
-# flake8: noqa
-import pdb
 from pandas.testing import assert_frame_equal
 
-import pandas as pd
 import pytest
-from unittest.mock import patch, Mock
-
-from sqlalchemy.exc import IntegrityError
 
 from invisible_flow.copa.data_officer_allegation import DataOfficerAllegation
 from manage import db
 from invisible_flow.constants import COPA_DB_BIND_KEY
 from tests.helpers.testing_data import transformed_data_with_rows, transformed_data_with_beat_id, \
     expected_transformed_data_with_beat_id, expected_load_data
-from tests.helpers.testing_data import transformed_data
 from invisible_flow.copa.loader import Loader
 from invisible_flow.copa.data_allegation import DataAllegation
-from invisible_flow.copa.data_allegation import insert_allegation_into_database
 
 
 class TestLoader:
@@ -42,16 +34,6 @@ class TestLoader:
         fourth_cr_id = transformed_data_with_beat_id['cr_id'][2]
         assert (queried_officer_data[3].allegation_id == fourth_cr_id)
 
-    # def test_load_data_with_matches_into_database(self):
-    #     insert_allegation_into_database(DataAllegation(cr_id="1087378"))
-    #     insert_allegation_into_database(DataAllegation(cr_id="1087387"))
-    #     testLoader = Loader()
-    #     queried_officer_data = DataOfficerAllegation.query.all()
-    #     pdb.set_trace()
-    #
-    #     testLoader.load_into_db(expected_transformed_data_with_beat_id)
-    #     assert (len(queried_officer_data) == len(DataOfficerAllegation.query.all()))
-
     def test_get_new_data(self):
         expected_new_data = expected_load_data
 
@@ -70,9 +52,6 @@ class TestLoader:
         testLoader.load_into_db(expected_transformed_data_with_beat_id)
 
         matches = testLoader.get_matches()
-
-        equality = matches.equals(expected_matches)
-        #pdb.set_trace()
 
         assert_frame_equal(matches, expected_matches, check_dtype=False)
 
