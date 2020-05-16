31515633c8c466808bf5fb6a97ba0e1721f5d7e0
j0vanka <jovanka.placeres@thoughtworks.com>
2020-05-16 18:39:23 -0500

    [#76] clean up; unused comments/code

--- invisible_flow/copa/saver.py
+++ invisible_flow/copa/saver.py
@@ -1,8 +1,3 @@
-# flake8: noqa
-import pdb
-from typing import List
-import os
-
 from invisible_flow.globals_factory import GlobalsFactory
 import pandas as pd
 
@@ -15,9 +10,5 @@ class Saver:
         self.storage = StorageFactory.get_storage()
 
     def save_to_csv(self, data_from_copa_scrape: pd.DataFrame, filename: str):
-        # outdir = f"local_upload/COPA_SCRAPE-{self.current_time}"
-        # if not os.path.exists(outdir):
-        #     os.mkdir(outdir)
-        # path_to_csv_storage = outdir + "/" + filename
         data_bytes = data_from_copa_scrape.to_csv(index=False).encode('utf-8')
         self.storage.store_byte_string(filename, data_bytes, f"COPA_SCRAPE-{self.current_time}")
