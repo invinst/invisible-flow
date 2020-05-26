c66c260d0dc45565c9533eafbe7bd6f3acdf3bea
Adam Kaplan <adam.kaplan@thoughtworks.com>
2020-05-25 23:43:28 -0700

    add commands to db reset scripts. these will need to be periodically updated if needing to constantly seed the db.

--- reset-db-staging.sh
+++ reset-db-staging.sh
@@ -5,6 +5,7 @@ partB=$(heroku pg:info --app invisiflow-staging | grep postgresql | cut -d "-" -
 partC=$(heroku pg:info --app invisiflow-staging | grep postgresql | cut -d "-" -f 4)
 
 ECHO "DELETE FROM data_allegation;" | heroku pg:psql $partA$partB-$partC --app invisiflow-staging
+ECHO "DELETE FROM data_officerallegation;" | heroku pg:psql $partA$partB-$partC --app invisiflow-staging
 
 ECHO -- Inserting data into database --
 ECHO "INSERT INTO data_allegation(cr_id, summary, add1, add2, city, is_officer_complaint, location, subjects, created_at, updated_at) VALUES('1003534', '', '', '?', 'CHICAGO ILLINOIS', false, 'other', '{}',  '2019-01-09 12:41:26.530261+08', '2019-01-09 12:41:31.205347+08');" | heroku pg:psql $partA$partB-$partC --app invisiflow-staging
