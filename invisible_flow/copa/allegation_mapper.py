807c09375f93099cbe0225f613d0b20a8123888b
Aaron North <aarontnorth@minint-1sr2fc9.corporate.thoughtworks.com>
2020-06-24 15:22:43 -0500

    [175][mob] refactor loader to mapper

--- invisible_flow/copa/allegation_mapper.py
+++ invisible_flow/copa/allegation_mapper.py
@@ -5,7 +5,7 @@ import pandas as pd
 from invisible_flow.copa.data_allegation import DataAllegation
 from manage import db
 
-class AllegationLoader:
+class AllegationMapper:
 
     def load_allegation_into_db(self, new_allegation_rows: pd.DataFrame()):
         db.session.bulk_insert_mappings(DataAllegation, new_allegation_rows.to_dict(orient="records"))
--- tests/copa/test_allegation_mapper.py
+++ tests/copa/test_allegation_mapper.py
@@ -3,13 +3,13 @@ import pdb
 import pytest
 
 from invisible_flow.constants import COPA_DB_BIND_KEY
-from invisible_flow.copa.allegation_loader import AllegationLoader
+from invisible_flow.copa.allegation_mapper import AllegationMapper
 import pandas as pd
 
 from invisible_flow.copa.data_allegation import DataAllegation
 from manage import db
 
-class TestAllegationLoader:
+class TestAllegationMapper:
 
     @pytest.fixture(autouse=True)
     def set_up(self):
@@ -18,15 +18,15 @@ class TestAllegationLoader:
         db.create_all(bind=COPA_DB_BIND_KEY)
 
     @pytest.mark.focus
-    def test_allegation_loader_should_load_new_rows(self):
-        test_loader = AllegationLoader()
+    def test_allegation_mapper_should_load_new_rows(self):
+        test_mapper = AllegationMapper()
 
         fake_new_rows = pd.DataFrame({
             "cr_id": ["33333333", "1111111", "999999", "100000", "100007"],
             "beat_id": ["111", "112", "114", "121", ""]
         })
 
-        test_loader.load_allegation_into_db(fake_new_rows)
+        test_mapper.load_allegation_into_db(fake_new_rows)
 
         queried_allegation_data = DataAllegation.query.all()
         #pdb.set_trace()
