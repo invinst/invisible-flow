bb07eaba37c75c32efa87bc2418127a7b1848504
Aaron North <aarontnorth@minint-1sr2fc9.corporate.thoughtworks.com>
2020-06-26 15:22:49 -0500

    [175][Aaron/Hayden/Joel]Refactored db queries from sorter to mapper

--- invisible_flow/copa/allegation_mapper.py
+++ invisible_flow/copa/allegation_mapper.py
@@ -3,9 +3,28 @@ import pdb
 import pandas as pd
 
 from invisible_flow.copa.data_allegation import DataAllegation
+from invisible_flow.copa.existing_crid import ExistingCrid
+from sqlalchemy.orm.exc import NoResultFound
+
 from manage import db
 
 class AllegationMapper:
+
+    def query_existing_crid_table(self):
+        try:
+            existing_crids = ExistingCrid.query.one().existing_crids
+        except NoResultFound:
+            existing_crids = ''
+        return existing_crids
+
+    def save_new_crids_to_db(self,old_crids,new_crids):
+        old_crids_str = ','.join(old_crids)
+        new_crids_str = ','.join(new_crids)
+        concatenated_crids = f"{old_crids_str},{new_crids_str}"
+        existing_crid = ExistingCrid.query.one()
+        existing_crid.existing_crids = concatenated_crids
+        db.session.add(existing_crid)
+        db.session.commit()
 
     def load_allegation_into_db(self, new_allegation_rows: pd.DataFrame()):
         db.session.bulk_insert_mappings(DataAllegation, new_allegation_rows.to_dict(orient="records"))
